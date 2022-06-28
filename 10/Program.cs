21d5cf88c1a96f2a4aed8f9f304de6d270953980
Denis Shaymardanov <d9655352866@gmail.com>
2022-06-28 14:49:48 +0500

    added task 10

---
+++ 10/Program.cs
@@ -0,0 +1,18 @@
+﻿int SecondDigit(int number)
+{
+    int result = number/10%10;
+    return result;
+}
+
+bool numIsCorrect(int number)
+{
+    int quotient = number/100;
+
+    bool result = (quotient != 0) & (quotient < 10);
+    return result;
+}
+
+Console.WriteLine("Введите трёхзначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+if (numIsCorrect(num)) Console.WriteLine("Вторая цифра введённого Вами числа - " + SecondDigit(num));
+else Console.WriteLine("Вы ввели некорректное число");
