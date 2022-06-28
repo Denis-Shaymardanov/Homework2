3e20d40ee128e286c12317e44e69d2ffeec9afc5
Denis Shaymardanov <d9655352866@gmail.com>
2022-06-28 14:59:57 +0500

    added comment in task10

--- 10/Program.cs
+++ 10/Program.cs
@@ -1,4 +1,8 @@
-﻿int SecondDigit(int number)
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+int SecondDigit(int number)
 {
     int result = number/10%10;
     return result;
